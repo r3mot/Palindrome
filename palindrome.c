fe00ba8a3bcb5d9ad71078b502beac0450c98559
Corey <mendrellac@gmail.com>
2022-04-30 13:05:33 -0700

    Added method to remove special characters and spaces from user input. Dynamically allocated memory for two pointers to char arrays to store data

--- palindrome.c
+++ palindrome.c
@@ -7,11 +7,18 @@
 #define CAPACITY 128
 
 int get_input(char*);
+int clean_input(char*, char*);
 
 /* Start of main program */
 int main() 
 {
-	get_input(NULL);
+
+	char* input = calloc(CAPACITY, sizeof(char));
+	char* cleaned = calloc(CAPACITY, sizeof(char));
+
+	get_input(input);
+	clean_input(input, cleaned);
+
 	return 0;
 
 }
@@ -36,5 +43,20 @@ int get_input(char* input)
 	}
 	input[i] = '\0';
 	return i;
+}
 
+/* Removes spaces & special characters from user-input */
+int clean_input(char* input, char* cleaned) 
+{
+	int i, j = 0;
+
+	for (i = 0; i < strlen(input); i++) 
+	{
+		if ((!isspace(input[i])) && isalpha(input[i]))
+		{
+			cleaned[j] = tolower(input[i]);
+			j++;
+		}
+	}
+	return j;
 }
