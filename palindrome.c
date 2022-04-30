bc5f9800e40dba6d5aa47c1c40a299f33f7faed8
Corey <mendrellac@gmail.com>
2022-04-30 13:01:23 -0700

    Added method to read user input

--- palindrome.c
+++ palindrome.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-int main() {
+#define CAPACITY 128
 
-	// test initial
+int get_input(char*);
 
+/* Start of main program */
+int main() 
+{
+	get_input(NULL);
 	return 0;
+
+}
+
+/* Reads user-input */
+int get_input(char* input) 
+{
+
+	printf("Please enter a word: ");
+
+	int ch, i = 0;
+
+	while (isspace(ch = getchar()));
+
+	while (ch != '\n' && ch != EOF) 
+	{
+		if (i < CAPACITY) 
+		{
+			input[i++] = ch;
+		}
+		ch = getchar();
+	}
+	input[i] = '\0';
+	return i;
 
 }
